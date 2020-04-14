761978bdf21b7c8a442cc1876f123ba4b1a0700d
Emilio <emiliocortina@gmail.com>
2020-04-14 13:26:13 +0200

    Exporting library services

--- projects/phoenix-library/src/public-api.ts
+++ projects/phoenix-library/src/public-api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/phoenix-library.module';
 export * from './lib/components/event-display/event-display.component';
+export * from './lib/services/eventdisplay.service';
+export * from './lib/services/ui.service';
